7f239456a8c1d2326f83b67cd674cac23bb10da6
vipafattal <vipafattal@gmail.com>
2018-08-05 17:24:13 +0300

    MutableFilter.kt & AddWhile.kt file added

--- app/src/main/java/com/codeboxlib/example/MainActivity.kt
+++ app/src/main/java/com/codeboxlib/example/MainActivity.kt
@@ -2,6 +2,8 @@ package com.codeboxlib.example
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.codebox.lib.android.utils.screenHelpers.halfScreenX
+import com.codebox.lib.android.utils.screenHelpers.halfScreenY
 import com.codebox.lib.android.widgets.snackbar.snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -10,9 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        snackbar("Hi")
 
-        button.setOnClickListener {
-        }
+
     }
 }
