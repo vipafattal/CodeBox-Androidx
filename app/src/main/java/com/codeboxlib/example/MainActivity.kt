06efbcb08f02c2b58c89f2669e244507017c8bfd
vipafattal <vipafattal@gmail.com>
2018-08-19 17:00:43 +0300

    new scale animator and new callbacks

--- app/src/main/java/com/codeboxlib/example/MainActivity.kt
+++ app/src/main/java/com/codeboxlib/example/MainActivity.kt
@@ -2,19 +2,13 @@ package com.codeboxlib.example
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.codebox.lib.android.widgets.snackbar.material
-import com.codebox.lib.android.widgets.snackbar.showAction
-import com.codebox.lib.android.widgets.snackbar.snackbar
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        snackbar("Item Added").material().showAction("Undo") {
-            textView.text = "Hello & Welcome"
-        }
 
     }
+
 }
